e3f2fe2a3fb4ecb5b90be619bb6daaa88d0ad6fa
sebahsr <sebahsr@gmail.com>
2024-07-18 10:53:27 +0000

    initaial commit

--- index.js
+++ index.js
@@ -2,14 +2,53 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const shortid = require('shortid');
+const validUrl = require('valid-url');
+const dns = require('dns');
+const urlParser = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+const urlDatabase = {};
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  const hostname = urlParser.parse(originalUrl).hostname;
+  console.log(hostname)
+  if(!hostname){
+    return res.json({ error: 'invalid url' });
+  }
+  if (!validUrl.isUri(originalUrl)) {
+    return res.json({ error: 'invalid url' });
+  }
 
+  dns.lookup(hostname, (err, address, family) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    const shortUrl = shortid.generate();
+    urlDatabase[shortUrl] = originalUrl;
+    res.json({ original_url: originalUrl, short_url: shortUrl });
+  });
+});
+
+// API endpoint to redirect to the original URL
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlDatabase[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.status(404).json({ error: 'No short URL found for the given input' });
+  }
+});
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
